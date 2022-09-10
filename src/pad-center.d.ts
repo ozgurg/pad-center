05678630e5ee1328a1c0aa32144e1e244b06cfb5
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2022-09-10 05:35:15 +0300

    Update pad-center.d.ts

--- src/pad-center.d.ts
+++ src/pad-center.d.ts
@@ -1,6 +1,11 @@
 /**
- * @param {{startWith: (string | number), endWith: (string | number), fillWith: (string | number), length: number}} params
- * @return {string}
+ * @param {{
+ * startWith: (string | number),
+ * endWith: (string | number),
+ * fillWith: (string | number),
+ * length: number
+ * }} params
+ * @returns {string}
  */
 export function padCenter(params: {
     startWith: (string | number);
